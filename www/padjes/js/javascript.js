477ad86249d4a4312ac983be9121354d931f5756
GERWINPC\Gerwin <gerwinputtenstein@gmail.com>
2017-02-25 14:02:44 +0100

    Initial commit with template website from startbootstrap.com

---
+++ www/padjes/js/javascript.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Gerwin on 24-2-2017.
+ */
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
