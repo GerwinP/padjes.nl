5b00262ac47aea23cf025e063974bf2155a72d81
GERWINPC\Gerwin <gerwinputtenstein@gmail.com>
2017-02-27 12:40:44 +0100

    Fixed the header bar changing color

--- www/padjes/js/javascript.js
+++ www/padjes/js/javascript.js
@@ -1,6 +1,30 @@
 /**
  * Created by Gerwin on 24-2-2017.
  */
+
+$(function() {
+    var $menu = $('#menu');
+    var $header = $('#header');
+    var addOrRemove = true;
+
+    $menu.on('click', function() {
+        if (addOrRemove) {
+            $header.addClass("toggle-red");
+            addOrRemove = false;
+        } else {
+            setTimeout(function() {
+                $header.removeClass("toggle-red");
+            }, 300);
+            addOrRemove = true;
+        }
+        //$header.toggleClass("toggle-red");
+    });
+
+    $menu.mouseup(function() {
+        $menu.blur();
+    });
+});
+
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
@@ -12,3 +36,4 @@ function collapseNavbar() {
 
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
+
