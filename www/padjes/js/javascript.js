866f7710eb452fcca2186bae89ba2536974ad946
josje <j.vantpadje@student.utwente.nl>
2018-02-07 11:00:35 +0100

    collage toegevoegd en meerdere sliders gemaakt

--- www/padjes/js/javascript.js
+++ www/padjes/js/javascript.js
@@ -37,3 +37,33 @@ function collapseNavbar() {
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
+	    // Initialize and Configure Magnific Popup Lightbox Plugin
+    $('.popup-gallery1').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+	
+	$('.popup-gallery2').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
